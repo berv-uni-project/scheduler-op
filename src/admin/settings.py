fa26b1b4a682052e06926fd8e45f53d815d5cd08
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2016-09-20 17:16:13 +0700

    Add config file

--- src/admin/settings.py
+++ src/admin/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-y*im9!+^lp)m$q5@#z*!zn&f+7+&&kdty1=1ss(teixn$ssv*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1:8000"]
 
 
 # Application definition
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        'FORM' : "scheduler_op/templates"
     },
 ]
 
