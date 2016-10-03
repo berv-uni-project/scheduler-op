45bb667c2960dccbe0f06fc91a659df8255a11fa
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2016-10-03 19:47:19 +0700

    update table init

--- src/scheduler_op/views.py
+++ src/scheduler_op/views.py
@@ -72,4 +72,4 @@ def result(request):
         #default hill climbing
 
     conflict = str(gettotalconflict(X.var))
-    return render(request,'result.html',{'X':X,'conflict':conflict,'allroom':a.roomlist});
+    return render(request,'result.html',{'X':X,'conflict':conflict,'allroom':a.roomlist,'time':range(7,18),'day':range(1,6)});
